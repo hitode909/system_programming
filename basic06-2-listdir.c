5b05ec85a8cbf0f479e0e01f48aa948badb29397
hitode909 <hitode909@gmail.com>
2010-11-03 13:39:33 +0900

    6-2

--- basic06-2-listdir.c
+++ basic06-2-listdir.c
@@ -13,7 +13,6 @@ int main(int argc, char *argv[])
 {
     DIR *dir;
     struct dirent *mydirent;
-    int i = 1;
 
     if(argc != 2) {
 	puts("USAGE: basic06-2-listdir <パス名>");
@@ -23,7 +22,7 @@ int main(int argc, char *argv[])
     if((dir = opendir(argv[1])) == NULL)
 	err_quit("opendir");
     while((mydirent = readdir(dir)) != NULL)
-	printf("%3d : %s\n", i++, mydirent->d_name);
+      printf("%3lld : %s\n", mydirent->d_ino, mydirent->d_name);
 
     closedir(dir);
     exit(EXIT_SUCCESS);
@@ -35,4 +34,14 @@ void err_quit(char *msg)
     exit(EXIT_FAILURE);
 }
 
-      
+/*
+2664269 : .
+1363979 : ..
+2664270 : .git
+3829185 : .gitignore
+2664332 : 1-1.c
+2667677 : 1-2-2.c
+2667787 : 1-2.c
+2664334 : 1-3.c
+2664335 : 1-5.c
+ */
