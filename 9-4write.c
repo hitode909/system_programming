96b92f6a84e6a8af386e41421f8753d58fd6b3ed
hitode909 <hitode909@gmail.com>
2010-11-24 14:03:02 +0900

    9-4

---
+++ 9-4write.c
@@ -0,0 +1,57 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+
+int main(int argc, char *argv[]){
+  int shmid;	/* 共有メモリ識別子 */
+  struct shmid_ds shminfo;	/* 共有メモリの情報 */
+  int bufsz;	/* メモリセグメントのサイズ */
+  char *shmaddr;	/* 共有メモリのアドレス */
+  int i;
+
+  srand(time(NULL));
+
+  /* 共有メモリ識別子を取得する */
+  if(argc != 2){
+    puts("USAGE: medium09-4 <shmid>");
+    exit(EXIT_FAILURE);
+  }
+  shmid = atoi(argv[1]);
+
+  /* 書き込みサイズを設定する */
+
+  if (shmctl(shmid, IPC_STAT, &shminfo) < 0) {
+    perror("shmctl");
+    exit(EXIT_FAILURE);
+  }
+  printf("size = %d\n", (int)shminfo.shm_segsz);
+  bufsz =  (int)shminfo.shm_segsz;
+
+  /* 読み書き両用で共有メモリにプロセスをアタッチする */
+  shmaddr = shmat(shmid, 0, 0);
+  if(shmaddr == (void *)(-1)){
+    perror("shmat");
+    exit(EXIT_FAILURE);
+  }
+
+  /* 共有メモリにデータを書き込み */
+  printf("write: ");
+  for(i=0; i<bufsz; i++){
+    *(shmaddr+i) = 'a' + rand() % ('z'-'a');
+    putc(*(shmaddr+i),stdout);
+  }
+  printf("\n");
+
+  /* プロセスをデタッチする */
+  if(shmdt(shmaddr)<0){
+    perror("shmdt");
+    exit(EXIT_FAILURE);
+  }
+
+  exit(EXIT_SUCCESS);
+}
