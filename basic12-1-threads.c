45dc1dbaefa757256ea65b86b3d97c5ddbdcd20d
hitode909 <hitode909@gmail.com>
2010-12-15 13:38:09 +0900

    12-1

--- basic12-1-threads.c
+++ basic12-1-threads.c
@@ -75,3 +75,18 @@ void do_wrap_up(int one_times, int another_times)
   printf("All done, one thing %d, another %d for a total of %d\n",
 	one_times, another_times, total);
 }
+
+/*
+basic12-1-threads.cにおいて，do_wrap_upの呼び出しの前にpthread_joinが必要となる理由は何か．
+
+threadの終了を待つため
+
+pthread joinは，指定したスレッドの終了を待つ関数である．
+
+        #include <pthread.h>
+        int pthread_join(pthread_t th, void **thread_return);
+        •   引数thで指定したスレッドが終了するのを待つ．
+        •   引数thread returnがNULLでない場合，thの返り値がこの引数で指し示される領域に格納される．
+        •   あるスレッドの終了を待つことができるスレッドは高々一つ(最大で一つ)である．すでに別のスレッドが終了を待っているスレッドthに対してpthread joinを呼び出すとエラーが返る．
+        •   返り値は，成功した場合0が返る．エラーの場合はエラーコードが 返る．
+ */
