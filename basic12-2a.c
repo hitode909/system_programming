7e045efb5bab1f9af35f555b702b689f49eaddba
hitode909 <hitode909@gmail.com>
2010-12-15 13:36:58 +0900

    12 sample

---
+++ basic12-2a.c
@@ -0,0 +1,57 @@
+﻿/*
+  basic12-2a.c
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <pthread.h>
+
+pthread_t thread;
+
+static int arg;
+static const int real_bad_error = -12;
+static const int normal_error = -10;
+static const int success = 1;
+
+void * routine_x(void *arg_in)
+{
+  int *arg = (int *)arg_in;
+
+  if (*arg == 1) {
+    pthread_exit((void *)real_bad_error);
+
+  } else if (*arg == 2) {
+    return ((void *)normal_error);
+
+  } else {
+    return ((void *)success);
+  }
+}
+
+extern int 
+main(int argc, char **argv)
+{
+  extern int arg;
+  pthread_t thread;
+  void *status;
+
+  if (argc < 2)
+    printf("usage: %s [1-3]\n", argv[0]), exit(1);
+
+  arg = atoi(argv[1]);
+
+  pthread_create(&thread,
+		 NULL,
+		 routine_x ,
+		 &arg);
+
+  pthread_join(thread, &status);
+  if (status == PTHREAD_CANCELED) {
+    printf("Thread was canceled.\n");
+  } else {
+    printf("Thread completed and exit status is %ld.\n", (int)status);
+  }
+
+  return 0;
+}
---
+++ medium12-4-pre.c
@@ -0,0 +1,47 @@
+﻿#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void *print_number(void *args);
+
+int num = 0;
+pthread_mutex_t mutex;
+
+/* boss thread */
+int main(void)
+{
+  pthread_t thread;
+
+  pthread_mutex_init(&mutex, NULL); /* init */
+  pthread_create(&thread, NULL, print_number, NULL); /* generating new thread */
+  while(1) {
+    int i;
+    pthread_mutex_lock(&mutex); /* lock */
+    for (i = 0; i < 5; i++) {
+      num++;
+      printf("boss thread: num = %d\n", num);
+      fflush(stdout);
+      sleep(1);
+    }
+    pthread_mutex_unlock(&mutex); /* unlock */
+  }
+  return(EXIT_FAILURE);
+}
+
+/* worker thread */
+void *print_number(void *args)
+{
+  while(1) {
+    int i;
+    pthread_mutex_lock(&mutex); /* lock */
+    for (i = 0; i < 5; i++) {
+      num++;
+      printf("worker thread: num = %d\n", num);
+      fflush(stdout);
+      sleep(2);
+    }
+    pthread_mutex_unlock(&mutex); /* unlock */
+  }
+  return(NULL);
+}
